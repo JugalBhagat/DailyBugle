6e9f79ba48985833fd3ae378978ad7c487b5c169
Jugal Bhagat <130580370+JugalBhagat@users.noreply.github.com>
2024-07-24 23:55:33 +0530

    Update spinner.js in src/componants (set width 40 to 45 of loader)

--- src/componants/spinner.js
+++ src/componants/spinner.js
@@ -3,7 +3,7 @@ import loading from './loading.gif'
 const spinner =()=>{
     return (
         <div className="text-center">
-            <img src={loading} alt="Loading..." className="loading-gif" width={40} />
+            <img src={loading} alt="Loading..." className="loading-gif" width={45} />
         </div>
     )
 }
